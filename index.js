d2f2378308a45915656140eabe36c9321f77032b
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-05-13 11:40:47 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,39 @@
+let haftakuni = Number (prompt("haftakuni krting"))
+
+// if(haftakuni === "1"){
+//     alert("Dushamba");
+// } 
+
+// if(haftakuni === "2"){
+//     alert("seshaba");
+// } 
+
+// if(haftakuni === "3"){
+//     alert("chorshamba");
+// } 
+
+// if(haftakuni === "4"){
+//     alert("pashamba");
+// } 
+
+// if(haftakuni === "5"){
+//     alert("juma");
+// }
+
+// if(haftakuni === "6"){
+//     alert("shamba");
+// }
+
+// if(haftakuni === "7"){
+//     alert("yakshamba");
+// }
+
+switch ( haftakuni){
+    case 1: alert("dusamba"); break;
+    case 2: alert("seshamba"); break;
+    case 3: alert("chorsamba");break;
+    case 4: alert("pashamba");break;
+    case 5: alert("juma");break;
+    case 6: alert("shamba");break;
+    case 7: alert("yakshamba");break;
+}
